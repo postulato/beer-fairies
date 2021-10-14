5b86e62d5fb88f6f09f3a3aeafb2e6d39e8e5084
postulato <cosadesgo@gmail.com>
2021-10-14 12:29:29 +0300

    app init

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCK4lbhMpEwNFIPKiHLn_ohb86oznD9_UA",
+    authDomain: "udemy-test-79e5c.firebaseapp.com",
+    databaseURL: "https://udemy-test-79e5c-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "udemy-test-79e5c",
+    storageBucket: "udemy-test-79e5c.appspot.com",
+    messagingSenderId: "592950922281",
+    appId: "1:592950922281:web:23c790ecdf8e23d0ca6038"
+  };
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {app} from './firebase';
+
+console.log(app);
+
+ReactDOM.render(
+  <React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
